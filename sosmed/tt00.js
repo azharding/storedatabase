5eea4304371f79fe6f052208c2f8718d715ba049
azharding <97221932+azharding@users.noreply.github.com>
2023-10-07 13:42:37 +0700

    Create tt00.js

---
+++ sosmed/tt00.js
@@ -0,0 +1,13 @@
+✧ followers tiktok♻️
+200 followers : 10k
+500 followers : 20k
+1000 followers : 35k
+
+✧ like tiktok⚡️
+200 like : 5k
+500 like : 10k
+1000 like : 15k
+
+1000 views tiktok : 250p
+500 share / save tiktok : 3k
+5 komentar tiktok [request] : 2k
