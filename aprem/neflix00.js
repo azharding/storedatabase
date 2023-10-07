ed751721281923643a84286eefef3992b35f3cf8
azharding <97221932+azharding@users.noreply.github.com>
2023-10-07 21:29:41 +0700

    Create neflix00.js

---
+++ aprem/neflix00.js
@@ -0,0 +1,26 @@
+*NETFLIX (BILL PKR LEGAL)*
+
+*✧ SHARING 1P2U*
+1 hari 2u : 1,5k 
+1 minggu 2u : 7k
+1 bulan 2u : 14,5k
+2 bulan 2u : 29k 
+3 bulan 2u : 44k
+
+*✧ SHARING 1P1U*
+1 minggu 1u : 10k
+1 bulan 1u : 28k 
+2 bulan 1u : 56k
+3 bulan 1u : 84k 
+
+*✧ PRIVATE*
+1 bulan bill PKR : 130k (bisa req pw)
+1 bulan bill rvisa : 122k (bisa req pw) 
+
+- sharing dan private akun dari store
+- durasi 2&3 renew / ganti akun 
+- jarang / hampir gapernah eror
+- all device bisa 
+- share bulanan & private bulanan bill pkr legal 
+- private bill rvisa tidak bisa ppj
+- share harian bill pkr legal / rvisa
