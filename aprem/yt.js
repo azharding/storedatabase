69b9b39e866ca752536427c326faeff4ec29dbc5
azharding <97221932+azharding@users.noreply.github.com>
2023-10-07 13:29:54 +0700

    Create yt.js

---
+++ aprem/yt.js
@@ -0,0 +1,27 @@
+*YOUTUBE*
+
+*✧ FAMPLAN* 
+1 bulan : 1,5k 
+2 bulan : 3k 
+3 bulan : 4,5k 
+4 bulan : 6k 
+
+*✧ INPLAN* 
+1 bulan : 3k 
+4 bulan : 12k 
+
+*✧ MIXPLAN* 
+3 bulan : 7k (1b indplan & 2b famplan) 
+6 bulan : 14k (4b indplan & 2b famplan) 
+
+*✧ FAMHEAD*
+1 bulan : 6k (bisa invite 5 anggota) 
+
+- *full garansi untuk akun yg backfree*
+- *AKUN KE DISABLE = NO GARANSI* 
+- *TIDAK MENERIMA COMPLAIN APAPUN JIKA AKUN KE DISABLE* 
+- *akun cust -> wajib akun yg fresh & ga ber verif* 
+- akun seller +2k
+- no renew untuk inplan (wajib fresh account), 
+renew untuk famplan (1 email cuma bisa 2x 
+Gabung keluarga, jika email sudah pernah gabung group keluarga 1x, maka kesempatan kmu gabung group keluarga sisa 1x, karna dalam 1 tahun cuma bisa 2x gabung group keluarga.❗ famplan diatas 2 bulan replace/ganti akun)
