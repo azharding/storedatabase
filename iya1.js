20a99c1a43a6fc89965a81fe39582f9a492618a3
azharding <97221932+azharding@users.noreply.github.com>
2023-10-07 13:38:50 +0700

    Delete iya1.js

--- iya1.js
+++
@@ -1,6 +0,0 @@
-const mobilelegends001 = `Stok Akun Lagi Kosong`
-const freefire001 = `Stok Akun Lagi Kosong`
-const codmobile001 = `Stok Akun Lagi Kosong`
-const ballpoll001 = `Stok Akun Lagi Kosong`
-const pointblank001 = `Stok Akun Lagi Kosong`
-const pubgmobile001 = `Stok Akun Lagi Kosong`
