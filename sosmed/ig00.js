acb367c3f610a8612063efe14c0401186fc37218
azharding <97221932+azharding@users.noreply.github.com>
2023-10-07 13:41:02 +0700

    Create ig00.js

---
+++ sosmed/ig00.js
@@ -0,0 +1,36 @@
+✧ followers instagram bot
+500 followers : 5k
+1000 followers : 10k
+2500 followers : 20k
+4000 folls + bonus 1000 folls : 35k
+↳ no gɑrɑnsi & bisɑ drop kɑpɑn sɑjɑ📛
+
+✧ followers instagram random♻️
+200 followers : 6k
+500 followers : 12k
+1000 followers : 23k
+2000 followers : 42k
+5000 folls + bonus 1000 folls : 100k
+↳ bergɑrɑnsi, non drop & best seller🔥
+
+✧ followers instagram aktf indonesia🇮🇩
+200 followers : 10k
+500 followers : 25k
+1000 followers : 50k
+↳ high quɑlity, penurunɑn hɑnyɑ terjɑdi jikɑ diunfoll karenɑ ɑkun reɑl indonesiɑ & fɑst proses⚡️
+
+✧ like instagram♻️
+200 like : 2k
+500 like : 4k
+1000 like : 8k
+2000 like + bonus 1000 like : 20k
+↳ bergɑrɑnsi, non drop / permɑnent & fɑst proses 1-5 menit💥
+
+✧ like instagram aktif indonesia🇮🇩
+200 like : 6k
+500 like : 12k
+1000 like : 23k
+↳ ɑkun reɑl indonesiɑ, non drop & fɑst proses 5-10 menit⚡️
+
+1000 views instagram : 1k
+100 komentar instagram [request] : 5k
