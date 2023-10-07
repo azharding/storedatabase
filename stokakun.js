422bc096ba535b19b0733fe1ee81d356e6d0ce11
azharding <97221932+azharding@users.noreply.github.com>
2023-10-07 11:13:38 +0700

    Update stokakun.js

--- stokakun.js
+++ stokakun.js
@@ -1,18 +1,6 @@
-fs = require('fs') 
-
-////////[STOCK AKUN]/////////////
 mobilelegends001 = `Stok Akun Lagi Kosong`
 freefire001 = `Stok Akun Lagi Kosong`
 codmobile001 = `Stok Akun Lagi Kosong`
 ballpoll001 = `Stok Akun Lagi Kosong`
 pointblank001 = `Stok Akun Lagi Kosong`
 pubgmobile001 = `Stok Akun Lagi Kosong`
-
-
-///////////////////////////////////////////////
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-fs.unwatchFile(file)
-delete require.cache[file]
-require(file)
-})
