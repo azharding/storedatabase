e0313579574f61983050af42cd25ee760418348b
azharding <97221932+azharding@users.noreply.github.com>
2023-10-07 13:41:36 +0700

    Create fb00.js

---
+++ sosmed/fb00.js
@@ -0,0 +1,15 @@
+*🚦FACEBOOK🚦*
+- 🔰500 Followers | Rp. 5.000
+- 🔰1000 Followers | Rp. 10.000
+- 🔰1500 Followers | Rp. 15.000
+- 🔰2000 Followers | Rp. 20.000
+- 🔰5000 Followers | Rp. 45.000
+- 🔶750 Likes | Rp. 5.000
+- 🔶1500 Likes | Rp. 10.000
+- 🔶2250 Likes | Rp. 15.000
+- 🔶3000 Likes | Rp. 20.000
+- 🔶5000 Likes | Rp. 30.000
+- 👁 7.500 Views | Rp. 10.000
+- 👁 15.000 Views | Rp. 20.000
+- 👁 50.000 Views | Rp. 55.000
+✅Via username/link
