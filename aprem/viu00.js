6ba1ff5d5c6c61c6f7cc8df6e1564a121b9db325
azharding <97221932+azharding@users.noreply.github.com>
2023-10-07 21:33:26 +0700

    Create viu00.js

---
+++ aprem/viu00.js
@@ -0,0 +1,23 @@
+*VIU*
+
+*(Tidak bisa menggunakan Indihome/tsel)*
+
+*✧ PRIVATE BIASA* 
+1 bulan : 3k 
+3 bulan : 6k 
+6 bulan : 8k
+1 tahun : 10k 
+
+*✧ PRIVATE ANTI LIMIT* 
+1 bulan : 5k 
+3 bulan : 8k 
+6 bulan : 12k
+1 tahun : 17k 
+
+- akun dari seller 
+- bisa login TV, tapi tidak di rekomendasikan 
+karena resiko tanggung sendiri 
+- garansi hanya untuk akun9 backfree 
+- hanya bisa 1 device 
+- anti limit hanya mengurangi resiko limit 
+- untuk durasi 1 tahun garansi 6 bulan
