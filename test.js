afb0a32354298e57ae308663dc56cc37da49df30
azharding <97221932+azharding@users.noreply.github.com>
2023-10-07 11:16:45 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+mobilelegends001 = `Stok Akun Lagi Kosong`
+freefire001 = `Stok Akun Lagi Kosong`
+codmobile001 = `Stok Akun Lagi Kosong`
+ballpoll001 = `Stok Akun Lagi Kosong`
+pointblank001 = `Stok Akun Lagi Kosong`
+pubgmobile001 = `Stok Akun Lagi Kosong`
