ab7b4b48909d7b472ee5fc6ac4c591489b67b2e3
azharding <97221932+azharding@users.noreply.github.com>
2023-10-15 08:25:31 +0700

    Create aprem.js

---
+++ aprem/aprem.js
@@ -0,0 +1,37 @@
+❏━━━『 𝘼𝙋𝙇𝙄𝙆𝘼𝙎𝙄 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 』═┅═━◧
+┣» 🛍️ NETFLIX (Net) ✅
+┣» 🛍️ SPOTIFY (spot) ✅
+┣» 🛍️ WETV (wet) ✅
+┣» 🛍️ DISNEY (Disn) ✅
+┣» 🛍️ YOUTUBE (Yt) ✅
+┣» 🛍️ VIU (viu) ✅
+┣» 🛍️ IQIYI (Iqi) ✅
+┣» 🛍️ IFLIX (Ifl) ✅
+┣» 🛍️ CANVA (canv) ✅
+┣» 🛍️ VIDIO (vidio) ✅
+┣» 🛍️ AMAZON (Amaz) ✅
+┣» 🛍️ REMINI (Remini) ✅
+┣» 🛍️ PICSART  (Piart) ❎
+┣» 🛍️ ALIGHT MOTION (Almo) ❎
+┣» 🛍️ GRAMMARLY (Gramm) ❎
+┣» 🛍️ WATTPAD (Watt) ❎
+┣» 🛍️ BSTATION (bst) ❎
+┣» 🛍️ LIGHTROOM (Lroom) ❎
+┣» 🛍️ SCRIBD (Scrib) ❎
+┣» 🛍️ VSCO (vsco) ❎
+┣» 🛍️ CAPCUT (Cc) ❎
+┣» 🛍️ TEZZA (Tza) ❎
+┣» 🛍️ SUSHIROLL (Ssl) ❎
+┣» 🛍️ GENFLIX (Genf) ❎
+┣» 🛍️ YOUKU (Yku) ❎
+┣» 🛍️ INSHOT (Ist) ❎
+┣» 🛍️ HBO GO (hbo) ❎
+┣» 🛍️ DAZZCAM (Dcam) ❎
+┣» 🛍️ TELEGRAM (Tele) ❎
+┣» 🛍️ COIN LINE (Cone) ❎
+┗━═┅═━–––––––––––––––๑
+Note : Status Produk Ini 
+✅ = Produk Ada
+❎ = Produk Habis
+♻️ = Produk Restok
+silahkan ketik salah satu aplikasi yang ingin di lihat misal youtube ketik saja "yt".
